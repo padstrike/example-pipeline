395040b74094db9dc4c689207a0b6b01c73bbf07
Poramet Promkorn <poramet.p@cimbthai.com>
2025-05-14 15:31:28 +0700

    add test job

---
+++ jobs/POC_pipeline.groovy
@@ -0,0 +1,31 @@
+pipelineJob('POC/DemoPipeline') {
+  description('POC mock pipeline job for testing JCasC + Job DSL')
+  definition {
+    cps {
+      script('''
+        pipeline {
+          agent any
+          stages {
+            stage('Init') {
+              steps {
+                echo 'Preparing environment...'
+              }
+            }
+            stage('Build') {
+              steps {
+                echo 'Running mock build...'
+              }
+            }
+            stage('Test') {
+              steps {
+                echo 'Executing mock tests...'
+              }
+            }
+          }
+        }
+      '''.stripIndent())
+      sandbox(true)
+    }
+  }
+  disabled(false)
+}
